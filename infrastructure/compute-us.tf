4439035635960bbbbb04d7aa86aa1464306b1a0a
Limster24 <lukelim@gmail.com>
2023-10-31 00:33:26 +0800

    ansible attempt 11

--- infrastructure/compute-us.tf
+++ infrastructure/compute-us.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "us_instance" {
 }
 
 resource "aws_instance" "sg_instance" {
-  provider      = aws.sg_provider
-  ami           = data.aws_ami.sg_vpn_server_image.id
+  provider      = aws.us_provider
+  ami           = data.aws_ami.us_vpn_server_image.id
   instance_type = "t2.micro"
   tags = {
     Type = "vpn_sg"
