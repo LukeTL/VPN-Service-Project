acdd0c5265c6a35cfb8bbf75cb1976790d97e7d4
24limster <lukelim24@gmail.com>
2023-10-30 11:13:03 +0800

    Test pipeline with provider

---
+++ infrastructure/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "1.6.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+
+  backend "http" {
+
+  }
+}
+
+
+provider "aws" {
+  alias  = "us_provider"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "sg_provider"
+  region = "ap-southeast-1"
+}
