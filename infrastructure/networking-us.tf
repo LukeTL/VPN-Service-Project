8538b37e64af99dfe3dc90053c6318bdb47ce240
LukeTL <ft.ggl.employee@gmail.com>
2023-11-06 21:59:47 +0800

    Testing infrastructure

--- infrastructure/networking-us.tf
+++ infrastructure/networking-us.tf
@@ -29,22 +29,19 @@ locals {
 
   # All US private subnets
   us_private_subnets = [for subnet in aws_subnet.us_subnets : subnet if subnet.map_public_ip_on_launch == false]
+
+  us_subnets = {
+    us_east_1a_private = aws_subnet.us_subnets[0],
+    us_east_1a_public = aws_subnet.us_subnets[1],
+    us_east_1b_private = aws_subnet.us_subnets[2],
+    us_east_1b_public = aws_subnet.us_subnets[3]
+  }
 }
 
 # USA Internet Gateway
 resource "aws_internet_gateway" "us_igw" {
   provider = aws.us_provider
   vpc_id   = aws_vpc.us_vpc.id
-}
-
-# Routing Table for Private USA Subnets
-resource "aws_route_table" "us_private_table" {
-  provider = aws.us_provider
-  vpc_id   = aws_vpc.us_vpc.id
-
-  tags = {
-    Name = "us_private_table"
-  }
 }
 
 # Routing Table for Public USA Subnets
@@ -62,15 +59,6 @@ resource "aws_route_table" "us_public_table" {
   }
 }
 
-# Associating Private Route Tables with Private Subnets
-resource "aws_route_table_association" "us_private" {
-  provider = aws.us_provider
-  count    = length(local.us_private_subnets)
-
-  route_table_id = aws_route_table.us_private_table.id
-  subnet_id      = local.us_private_subnets[count.index].id
-}
-
 # Associating Public Route Tables with Public Subnets
 resource "aws_route_table_association" "us_public" {
   provider = aws.us_provider
@@ -80,32 +68,44 @@ resource "aws_route_table_association" "us_public" {
   subnet_id      = local.us_public_subnets[count.index].id
 }
 
-resource "aws_eip" "us_east_1a_eip" {
+resource "aws_eip" "us_eips" {
+  count = length(local.us_private_subnets)
   provider = aws.us_provider
   depends_on = [ aws_internet_gateway.us_igw ]
 }
 
-resource "aws_eip" "us_east_1b_eip" {
+resource "aws_nat_gateway" "us_nats" {
+  count = 2
   provider = aws.us_provider
-  depends_on = [ aws_internet_gateway.us_igw ]
-}
-
-resource "aws_nat_gateway" "us_east_1a_nat" {
-  provider = aws.us_provider
-  allocation_id = aws_eip.us_east_1a_eip.id
-  subnet_id = local.us_public_subnets[0].id
+  allocation_id = aws_eip.us_eips[count.index].id
+  subnet_id = local.us_public_subnets[count.index].id
 
   tags = {
-    "Name" = "us_east_1a_nat"
+    "Name" = "${local.us_public_subnets[count.index]}-nat"
   }
 }
 
-resource "aws_nat_gateway" "us_east_1b_nat" {
-  provider = aws.us_provider
-  allocation_id = aws_eip.us_east_1b_eip.id
-  subnet_id = local.us_public_subnets[1].id
 
-  tags = {
-    "Name" = "us_east_1b_nat"
+# Routing Table for Private USA Subnets
+resource "aws_route_table" "us_private_table" {
+  count = length(local.us_private_subnets)
+  provider = aws.us_provider
+  vpc_id   = aws_vpc.us_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.us_nats[count.index].id
   }
+  tags = {
+    Name = "us_private_table"
+  }
+}
+
+# Associating Private Route Tables with Private Subnets
+resource "aws_route_table_association" "us_private" {
+  count    = length(local.us_private_subnets)
+  provider = aws.us_provider
+
+  route_table_id = aws_route_table.us_private_table[count.index].id
+  subnet_id      = local.us_private_subnets[count.index].id
 }
