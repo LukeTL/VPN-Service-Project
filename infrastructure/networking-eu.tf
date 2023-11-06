a1dbc5fb0f49e1bffe61b00e04e6d41ca3ec897c
LukeTL <ft.ggl.employee@gmail.com>
2023-11-06 22:37:00 +0800

    Completed NAT for USA & Ireland

--- infrastructure/networking-eu.tf
+++ infrastructure/networking-eu.tf
@@ -7,8 +7,9 @@ resource "aws_vpc" "eu_vpc" {
 }
 
 # Ireland Public and Private Subnets
+# Iterating through a variable containing a list of objects to generate subnets
 resource "aws_subnet" "eu_subnets" {
-  count = length(var.us_subnet_details)
+  count = length(var.eu_subnet_details)
 
   provider = aws.eu_provider
   vpc_id   = aws_vpc.eu_vpc.id
@@ -24,30 +25,26 @@ resource "aws_subnet" "eu_subnets" {
 
 # Intialising Ireland subnets as local variables
 locals {
-  # All Ireland public subnets
-  eu_public_subnets  = [for subnet in aws_subnet.eu_subnets : subnet if subnet.map_public_ip_on_launch]
-
-  # All Ireland private subnets
+  eu_public_subnets = [for subnet in aws_subnet.eu_subnets : subnet if subnet.map_public_ip_on_launch]
   eu_private_subnets = [for subnet in aws_subnet.eu_subnets : subnet if subnet.map_public_ip_on_launch == false]
+
+  eu_subnets = {
+    eu_west_1a_private = aws_subnet.eu_subnets[0],
+    eu_west_1a_public  = aws_subnet.eu_subnets[1],
+    eu_west_1b_private = aws_subnet.eu_subnets[2],
+    eu_west_1b_public  = aws_subnet.eu_subnets[3]
+  }
 }
 
 # Ireland Internet Gateway
+# IGW is region-scoped, public subnets will share same gateway
 resource "aws_internet_gateway" "eu_igw" {
   provider = aws.eu_provider
   vpc_id   = aws_vpc.eu_vpc.id
 }
 
-# Routing Table for Private Ireland Subnets
-resource "aws_route_table" "eu_private_table" {
-  provider = aws.eu_provider
-  vpc_id   = aws_vpc.eu_vpc.id
-
-  tags = {
-    Name = "eu_private_table"
-  }
-}
-
 # Routing Table for Public Ireland Subnets
+# Routing all outbound traffic in public subnets to IGW
 resource "aws_route_table" "eu_public_table" {
   provider = aws.eu_provider
   vpc_id   = aws_vpc.eu_vpc.id
@@ -62,16 +59,7 @@ resource "aws_route_table" "eu_public_table" {
   }
 }
 
-# Associating Private Route Tables with Private Subnets
-resource "aws_route_table_association" "eu_private" {
-  provider = aws.eu_provider
-  count    = length(local.eu_private_subnets)
-
-  route_table_id = aws_route_table.eu_private_table.id
-  subnet_id      = local.eu_private_subnets[count.index].id
-}
-
-# Associating Public Route Tables with Public Subnets
+# Associating the same Public Route Table to both Public Subnets
 resource "aws_route_table_association" "eu_public" {
   provider = aws.eu_provider
   count    = length(local.eu_public_subnets)
@@ -80,32 +68,43 @@ resource "aws_route_table_association" "eu_public" {
   subnet_id      = local.eu_public_subnets[count.index].id
 }
 
-resource "aws_eip" "eu_west_1a_eip" {
-  provider = aws.eu_provider
-  depends_on = [ aws_internet_gateway.eu_igw ]
+resource "aws_eip" "eu_eips" {
+  provider   = aws.eu_provider
+  count      = length(local.eu_private_subnets)
+  depends_on = [aws_internet_gateway.eu_igw]
 }
 
-resource "aws_eip" "eu_west_1b_eip" {
-  provider = aws.eu_provider
-  depends_on = [ aws_internet_gateway.eu_igw ]
-}
-
-resource "aws_nat_gateway" "eu_west_1a_nat" {
-  provider = aws.eu_provider
-  allocation_id = aws_eip.eu_west_1a_eip.id
-  subnet_id = local.eu_public_subnets[0].id
+resource "aws_nat_gateway" "eu_nats" {
+  provider      = aws.eu_provider
+  count         = length(local.eu_private_subnets)
+  allocation_id = aws_eip.eu_eips[count.index].id
+  subnet_id     = local.eu_public_subnets[count.index].id
 
   tags = {
-    "Name" = "eu_west_1a_nat"
+    "Name" = "${local.eu_public_subnets[count.index].availability_zone}-nat"
   }
 }
 
-resource "aws_nat_gateway" "eu_west_1b_nat" {
+# 2 Routing Tables for each Private Subnets to route to their respective NAT Gateways
+resource "aws_route_table" "eu_private_table" {
   provider = aws.eu_provider
-  allocation_id = aws_eip.eu_west_1b_eip.id
-  subnet_id = local.eu_public_subnets[1].id
+  count    = length(local.eu_private_subnets)
+  vpc_id   = aws_vpc.eu_vpc.id
 
-  tags = {
-    "Name" = "eu_west_1b_nat"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eu_nats[count.index].id
   }
+  tags = {
+    Name = "${local.eu_public_subnets[count.index].availability_zone}-private_table"
+  }
+}
+
+# Associating the Private Route Tables with their respective Private Subnets
+resource "aws_route_table_association" "eu_private" {
+  provider = aws.eu_provider
+  count    = length(local.eu_private_subnets)
+
+  route_table_id = aws_route_table.eu_private_table[count.index].id
+  subnet_id      = local.eu_private_subnets[count.index].id
 }
--- infrastructure/networking-us.tf
+++ infrastructure/networking-us.tf
@@ -6,11 +6,11 @@ resource "aws_vpc" "us_vpc" {
   cidr_block = var.us_vpc_cidr_block
 }
 
-# USA Public and Private Subnets
+# USA Public and Private Subnets"
+# Iterating through a variable containing a list of objects to generate subnets
 resource "aws_subnet" "us_subnets" {
-  count = length(var.us_subnet_details)
-
   provider = aws.us_provider
+  count    = length(var.us_subnet_details)
   vpc_id   = aws_vpc.us_vpc.id
 
   cidr_block              = var.us_subnet_details[count.index].subnet_cidr
@@ -23,28 +23,28 @@ resource "aws_subnet" "us_subnets" {
 }
 
 # Intialising USA subnets as local variables
+# Public & Private Subnets as a list, each individual subnet can be found in a map
 locals {
-  # All US public subnets
-  us_public_subnets = [for subnet in aws_subnet.us_subnets : subnet if subnet.map_public_ip_on_launch]
-
-  # All US private subnets
+  us_public_subnets  = [for subnet in aws_subnet.us_subnets : subnet if subnet.map_public_ip_on_launch]
   us_private_subnets = [for subnet in aws_subnet.us_subnets : subnet if subnet.map_public_ip_on_launch == false]
 
   us_subnets = {
     us_east_1a_private = aws_subnet.us_subnets[0],
-    us_east_1a_public = aws_subnet.us_subnets[1],
+    us_east_1a_public  = aws_subnet.us_subnets[1],
     us_east_1b_private = aws_subnet.us_subnets[2],
-    us_east_1b_public = aws_subnet.us_subnets[3]
+    us_east_1b_public  = aws_subnet.us_subnets[3]
   }
 }
 
 # USA Internet Gateway
+# IGW is region-scoped, public subnets will share same gateway
 resource "aws_internet_gateway" "us_igw" {
   provider = aws.us_provider
   vpc_id   = aws_vpc.us_vpc.id
 }
 
 # Routing Table for Public USA Subnets
+# Routing all outbound traffic in public subnets to IGW
 resource "aws_route_table" "us_public_table" {
   provider = aws.us_provider
   vpc_id   = aws_vpc.us_vpc.id
@@ -59,7 +59,7 @@ resource "aws_route_table" "us_public_table" {
   }
 }
 
-# Associating Public Route Tables with Public Subnets
+# Associating the same Public Route Table to both Public Subnets
 resource "aws_route_table_association" "us_public" {
   provider = aws.us_provider
   count    = length(local.us_public_subnets)
@@ -68,42 +68,46 @@ resource "aws_route_table_association" "us_public" {
   subnet_id      = local.us_public_subnets[count.index].id
 }
 
+# Creating 2 EIPs for 2 NAT Gateways
+# EIPS depend on IGW
 resource "aws_eip" "us_eips" {
-  count = length(local.us_private_subnets)
-  provider = aws.us_provider
-  depends_on = [ aws_internet_gateway.us_igw ]
+  provider   = aws.us_provider
+  count      = length(local.us_private_subnets)
+  depends_on = [aws_internet_gateway.us_igw]
 }
 
+# Creating a NAT Gateway in each of the 2 Public subnets for the 2 Private subnets
+# Set name of NAT Gateway based on AZ of Public Subnet
 resource "aws_nat_gateway" "us_nats" {
-  count = length(local.us_private_subnets)
-  provider = aws.us_provider
+  provider      = aws.us_provider
+  count         = length(local.us_private_subnets)
   allocation_id = aws_eip.us_eips[count.index].id
-  subnet_id = local.us_public_subnets[count.index].id
+  subnet_id     = local.us_public_subnets[count.index].id
 
   tags = {
     "Name" = "${local.us_public_subnets[count.index].availability_zone}-nat"
   }
 }
 
-# Routing Table for Private USA Subnets
+# 2 Routing Tables for each Private Subnets to route to their respective NAT Gateways
 resource "aws_route_table" "us_private_table" {
-  count = length(local.us_private_subnets)
   provider = aws.us_provider
+  count    = length(local.us_private_subnets)
   vpc_id   = aws_vpc.us_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.us_nats[count.index].id
   }
   tags = {
-    Name = "us_private_table"
+    Name = "${local.us_public_subnets[count.index].availability_zone}-private_table"
   }
 }
 
-# Associating Private Route Tables with Private Subnets
+# Associating the Private Route Tables with their respective Private Subnets
 resource "aws_route_table_association" "us_private" {
-  count    = length(local.us_private_subnets)
   provider = aws.us_provider
+  count    = length(local.us_private_subnets)
 
   route_table_id = aws_route_table.us_private_table[count.index].id
   subnet_id      = local.us_private_subnets[count.index].id
