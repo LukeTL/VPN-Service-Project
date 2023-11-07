947d611dff37d1e747c3b622c3a129f9dc7e05a8
LukeTL <ft.ggl.employee@gmail.com>
2023-11-07 23:21:34 +0800

    editted launch script

--- control_node_configuration/us_control_node/launch.sh
+++ control_node_configuration/us_control_node/launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "The value of MY_VARIABLE is: $MY_VARIABLE"
-#
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "The value of MY_VARIABLE is: $MY_VARIABLE" > /var/www/html/index.html
