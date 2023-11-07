8f61c0af1bf40c2760b84c60ab1acd66e9160de2
LukeTL <ft.ggl.employee@gmail.com>
2023-11-07 23:31:12 +0800

    Fix user.sh for us

--- control_node_configuration/us_control_node/launch.sh
+++ control_node_configuration/us_control_node/launch.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "The value of MY_VARIABLE is: $MY_VARIABLE" > /var/www/html/index.html
+echo "<h1>The value of MY_VARIABLE is: $MY_VARIABLE<h1>" > /var/www/html/index.html
