9fe16add89adb121a467205dbd7b6005a70f4212
LukeTL <ft.ggl.employee@gmail.com>
2023-11-09 00:05:48 +0800

    Testing env variables

--- infrastructure/compute-us.tf
+++ infrastructure/compute-us.tf
@@ -11,11 +11,16 @@
 # Bastions will need an EIP
 
 # Setup Bastions in Public subnets
+
+variable "CI_PROJECT_DIR" {
+  type = string
+}
+
 resource "aws_instance" "us_east_1a_bastion" {
   provider      = aws.us_provider
   ami           = data.aws_ami.us_vpn_server_image.id
   instance_type = "t2.micro"
-  user_data = filebase64("${CI_PROJECT_DIR}/control_node_configuration/us_control_node/launch.sh")
+  user_data = filebase64("${var.CI_PROJECT_DIR}/control_node_configuration/us_control_node/launch.sh")
   tags = {
     Type = "vpn_us"
   }
